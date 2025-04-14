352dc2bcd3e1b108f4c5b558387af4c5c242f52c
Himanshu Shukla <hshukla2611@gmail.com>
2025-04-14 11:19:23 +0530

    typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import { Quote } from "lucide-react";
+import Quote from "./pages/Quote";
 
 function App() {
     return (
